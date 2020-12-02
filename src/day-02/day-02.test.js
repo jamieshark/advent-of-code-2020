93cf6410c4bb463e6c988b89fff256bdb56c047e
jamieshark <jchung@atlassian.com>
2020-12-02 14:17:14 -0600

    Solved day 2.

--- src/day-02/day-02.test.js
+++ src/day-02/day-02.test.js
@@ -1,19 +1,29 @@
 const assert = require('assert');
 const fs = require('fs');
 
-const { funcA } = require('.');
+const { passwordCheck, passwordCheckPos } = require('.');
 
+const example = fs.readFileSync(`${__dirname}/example.txt`, 'utf-8');
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8');
 
 describe('day-02: Password Philosophy', () => {
   describe('Part One', () => {
-    it('should do something', () => {
-      const input = true;
-      assert.strictEqual(funcA(input), true);
+    it('should validate passwords with occurence', () => {
+      assert.strictEqual(passwordCheck(example), 2);
     });
 
     it('should solve the problem', () => {
-      assert.strictEqual(funcA(input), undefined);
+      assert.strictEqual(passwordCheck(input), 500);
+    });
+  });
+
+  describe('Part Two', () => {
+    it('should validate passwords with position', () => {
+      assert.strictEqual(passwordCheckPos(example), 1);
+    });
+
+    it('should solve the problem', () => {
+      assert.strictEqual(passwordCheckPos(input), 313);
     });
   });
 });
--- src/day-02/index.js
+++ src/day-02/index.js
@@ -1,5 +1,35 @@
 module.exports = {
-  funcA: (input) => {
-    return input;
+  passwordCheck: (input) => {
+    const lineItems = input.split('\n');
+    const validPasswords = lineItems
+      .map((item) => {
+        const [range, target, string] = item.split(' ');
+        const targetMatch = string.match(new RegExp(target.substr(0, 1), 'g'));
+        const targetOccurence = targetMatch ? targetMatch.length : 0;
+        const [min, max] = range.split('-');
+
+        return targetOccurence >= min && targetOccurence <= max;
+      })
+      .filter(Boolean);
+
+    return validPasswords.length;
+  },
+  passwordCheckPos: (input) => {
+    const lineItems = input.split('\n');
+    const validPasswords = lineItems
+      .map((item) => {
+        const [targetPos, target, string] = item.split(' ');
+        const [firstPos, secondPos] = targetPos.split('-');
+        const targetChar = target.substr(0, 1);
+        // adjust for index 0
+        const firstMatch = string.substr(firstPos - 1, 1) === targetChar;
+        const secondMatch = string.substr(secondPos - 1, 1) === targetChar;
+
+        // valid if there is _not_ more than one, but at least one.
+        return !(firstMatch && secondMatch) && (firstMatch || secondMatch);
+      })
+      .filter(Boolean);
+
+    return validPasswords.length;
   },
 };
